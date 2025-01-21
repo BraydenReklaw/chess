49116de1b2816a50e08581f2894321c1bbb38c4d
BraydenW <braydenthewalker@gmail.com>
2025-01-21 11:47:47 -0700

    ChessPosition updated. All tests pass

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents a single square position on a chess board
  * <p>
@@ -31,4 +33,17 @@ public class ChessPosition {
     public int getColumn() {
         return col;
     }
+
+    public boolean equals (Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        ChessPosition that = (ChessPosition) obj;
+        return row == that.row && col == that.col;
+    }
+
+
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
 }
