c20be0752075b6bf2f1bc44f1a48f6b919c866bc
BraydenW <braydenthewalker@gmail.com>
2025-01-21 13:02:25 -0700

    Fixed Overrides in ChessBoard and generated Overrides in ChessPiece. Reset Board now passes

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -80,6 +80,7 @@ public class ChessBoard {
 
     @Override
     public boolean equals(Object o) {
+        if (this == o) return true;
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
