3c22c349c84e65e25f49d4eceace96123e2d1677
BraydenW <braydenthewalker@gmail.com>
2025-01-22 11:33:25 -0700

    QueenMoveCalculator implemented. Relevent tests pass

--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -7,7 +7,35 @@ public class QueenMoveCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
-        // logic
+        int [][] possibleMoves = {
+                {1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}
+                // up, right, down, left, up-right, down-right, down-left, up-left
+        };
+
+        for (int i = 0; i < possibleMoves.length; i++) {
+            int[] possibleMove = possibleMoves[i];
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            while (true) {
+                row += possibleMove[0];
+                col += possibleMove[1];
+                if (!board.isValidMove(row, col)) {
+                    break;
+                }
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                else {
+                    if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                    break;
+                }
+            }
+
+        };
         return moves;
     }
 }
