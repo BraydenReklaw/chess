ef4cab3a8d16de01bca0dc4330fa088ec1c8e8ae
BraydenW <braydenthewalker@gmail.com>
2025-01-22 12:06:56 -0700

    PawnMoveCalculator starting move implemented for White

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -7,7 +7,37 @@ public class PawnMoveCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
-        // logic
+        int [][] possibleWhiteMoves = {
+                {1,0}, //up
+                {1,-1}, //attack left
+                {1,1}, //attack right
+
+        };
+        int [][] possibleBlackMoves = {
+                {-1,0}, //down
+                {-1,-1}, // attack-left
+                {-1,1}, // attack-right
+        };
+
+        ChessPiece myPawn = board.getPiece(myPosition);
+        int[][] possibleMoves = myPawn.getTeamColor() == ChessGame.TeamColor.WHITE ? possibleWhiteMoves : possibleBlackMoves;
+        for (int i = 0; i < possibleMoves.length; i++) {
+            int[] possibleMove = possibleMoves[i];
+            int row = myPosition.getRow() + possibleMove[0];
+            int col = myPosition.getColumn() + possibleMove[1];
+            ChessPosition newPosition = new ChessPosition(row, col);
+            if (myPosition.getRow() == 2) {
+                if (board.getPiece(newPosition) == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                ChessPosition pawnsprint = new ChessPosition(row + 1, col);
+                if (board.getPiece(pawnsprint) == null ||(board.getPiece(pawnsprint).getTeamColor() != board.getPiece(myPosition).getTeamColor())){
+                    moves.add(new ChessMove(myPosition, pawnsprint, null));
+                }
+            }
+        }
         return moves;
     }
 }
+
+// ChessPiece.PieceType.QUEEN
