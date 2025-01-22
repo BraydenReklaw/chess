413b5b8a12b438bbe9f38beb53ceace67989618a
BraydenW <braydenthewalker@gmail.com>
2025-01-22 11:34:49 -0700

    KingMoveCalculator implemented. Relevent tests pass

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -7,7 +7,23 @@ public class KingMoveCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new ArrayList<>();
-        // logic
+        int [][] possibleMoves = {
+                {1,0}, {0,1}, {-1,0}, {0,-1}, {1,1}, {1,-1}, {-1,1}, {-1,-1}
+                // up, right, down, left, up-right, down-right, down-left, up-left
+        };
+        for (int i = 0; i < possibleMoves.length; i++) {
+            int[] possibleMove = possibleMoves[i];
+            int row = myPosition.getRow() + possibleMove[0];
+            int col = myPosition.getColumn() + possibleMove[1];
+            if (board.isValidMove(row, col)) {
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null || (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor())) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+
+        }
         return moves;
     }
 }
