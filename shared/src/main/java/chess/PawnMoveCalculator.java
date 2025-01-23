74fd4571aca15795767e482d4e97da257aef6b2b
BraydenW <braydenthewalker@gmail.com>
2025-01-22 17:33:48 -0700

    PawnMoveCalculator mid re-implementation utilizing boolean flags

--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -28,24 +28,49 @@ public class PawnMoveCalculator implements PieceMovesCalculator {
             int col = myPosition.getColumn() + possibleMove[1];
             ChessPosition newPosition = new ChessPosition(row, col);
             boolean promote = false;
-            if (row == 8) {promote = true;}
-            if (i == 0) {
-                // pawn forward moves from start space
-                if (myPosition.getRow() == 2) {
-                    if (board.getPiece(newPosition) == null) {
+            boolean start = false;
+            if (row == 8 || row == 1) {
+                promote = true;
+            }
+            if (row == 2 || row == 7) {
+                start = true;
+            }
+            if (!promote) {
+                if (board.isValidMove(row, col)) {
+                    ChessPiece piece = board.getPiece(newPosition);
+                    if (piece == null && i == 0) {
                         moves.add(new ChessMove(myPosition, newPosition, null));
-                        ChessPosition pawnsprint = new ChessPosition(row + 1, col);
-                        if (board.getPiece(pawnsprint) == null || (board.getPiece(pawnsprint).getTeamColor() != board.getPiece(myPosition).getTeamColor())) {
-                            moves.add(new ChessMove(myPosition, pawnsprint, null));
+                        if (start) {
+                            // implement advance position here
+                            piece = board.getPiece(newPosition + );
+                            if (piece == null && i == 0) {
+                                moves.add(new ChessMove(myPosition, newPosition, null));
                         }
                     }
-                }
-                else {
-                    if (board.getPiece(newPosition) == null && !promote) {
+                    if (i != 0 && piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
                         moves.add(new ChessMove(myPosition, newPosition, null));
                     }
                 }
             }
+
+
+//            if (i == 0) {
+//                // pawn forward moves from start space
+//                if (myPosition.getRow() == 2) {
+//                    if (board.getPiece(newPosition) == null) {
+//                        moves.add(new ChessMove(myPosition, newPosition, null));
+//                        ChessPosition pawnsprint = new ChessPosition(row + 1, col);
+//                        if (board.getPiece(pawnsprint) == null || (board.getPiece(pawnsprint).getTeamColor() != board.getPiece(myPosition).getTeamColor())) {
+//                            moves.add(new ChessMove(myPosition, pawnsprint, null));
+//                        }
+//                    }
+//                }
+//                else {
+//                    if (board.getPiece(newPosition) == null && !promote) {
+//                        moves.add(new ChessMove(myPosition, newPosition, null));
+//                    }
+//                }
+//            }
 
         }
         return moves;
