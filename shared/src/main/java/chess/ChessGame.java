ec453d63650603a94356a499080f26febfaeacac
BraydenW <braydenthewalker@gmail.com>
2025-02-03 10:56:32 -0700

    ChessGame implemented with initial conditions, newGame() test passes

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -10,15 +10,21 @@ import java.util.Collection;
  */
 public class ChessGame {
 
-    public ChessGame() {
+    private ChessBoard gameBoard;
+    private TeamColor turnColor;
 
+    public ChessGame() {
+        this.gameBoard = new ChessBoard();
+        this.gameBoard.resetBoard();
+        this.turnColor = TeamColor.WHITE;
     }
 
     /**
      * @return Which team's turn it is
      */
     public TeamColor getTeamTurn() {
-        throw new RuntimeException("Not implemented");
+        return turnColor;
+        // throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -27,7 +33,8 @@ public class ChessGame {
      * @param team the team whose turn it is
      */
     public void setTeamTurn(TeamColor team) {
-        throw new RuntimeException("Not implemented");
+        this.turnColor = team;
+        //throw new RuntimeException("Not implemented");
     }
 
     /**
@@ -96,6 +103,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
+        this.gameBoard = board;
         throw new RuntimeException("Not implemented");
     }
 
@@ -105,6 +113,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return gameBoard;
+        // throw new RuntimeException("Not implemented");
     }
 }
