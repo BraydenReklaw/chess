9be1a7ff5e286301ea157566b233ab404d9803e1
BraydenW <braydenthewalker@gmail.com>
2025-03-02 16:08:53 -0700

    Queen, Rook and Bishop shared logic implemented

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -6,36 +6,10 @@ import java.util.Collection;
 public class BishopMoveCalculator implements PieceMovesCalculator {
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        Collection<ChessMove> moves = new ArrayList<>();
         int [][] possibleMoves = {
                 {1,1}, {1,-1}, {-1,1}, {-1,-1}
                 // up-right, down-right, down-left, up-left
         };
-
-        for (int i = 0; i < possibleMoves.length; i++) {
-            int[] possibleMove = possibleMoves[i];
-            int row = myPosition.getRow();
-            int col = myPosition.getColumn();
-            while (true) {
-                row += possibleMove[0];
-                col += possibleMove[1];
-                if (!board.isValidMove(row, col)) {
-                    break;
-                }
-                ChessPosition newPosition = new ChessPosition(row, col);
-                ChessPiece piece = board.getPiece(newPosition);
-                if (piece == null) {
-                    moves.add(new ChessMove(myPosition, newPosition, null));
-                }
-                else {
-                    if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()){
-                        moves.add(new ChessMove(myPosition, newPosition, null));
-                    }
-                    break;
-                }
-            }
-
-        };
-        return moves;
+        return SharedMoveLogic.queenRookBishopSharedLogic(possibleMoves, board, myPosition);
     }
 }
--- shared/src/main/java/chess/SharedMoveLogic.java
+++ shared/src/main/java/chess/SharedMoveLogic.java
@@ -22,4 +22,33 @@ public class SharedMoveLogic {
         }
         return moves;
     }
+
+    public static Collection<ChessMove> queenRookBishopSharedLogic(int [][] possibleMoves, ChessBoard board, ChessPosition myPosition) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (int i = 0; i < possibleMoves.length; i++) {
+            int[] possibleMove = possibleMoves[i];
+            int row = myPosition.getRow();
+            int col = myPosition.getColumn();
+            while (true) {
+                row += possibleMove[0];
+                col += possibleMove[1];
+                if (!board.isValidMove(row, col)) {
+                    break;
+                }
+                ChessPosition newPosition = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+                else {
+                    if (piece.getTeamColor() != board.getPiece(myPosition).getTeamColor()){
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                    break;
+                }
+            }
+
+        }
+        return moves;
+    }
 }
