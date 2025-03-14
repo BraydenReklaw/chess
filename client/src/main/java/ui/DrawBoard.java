45f6e097c9ccf4691e68c719b929d274b10dc706
BraydenW <braydenthewalker@gmail.com>
2025-03-14 11:13:32 -0600

    board new prints correctly checkered and positions labeled

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,6 +1,8 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
@@ -12,7 +14,7 @@ public class DrawBoard {
 
     public static void main(String[] args) {
         var printOut = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-        String playerColor = "WHITE";
+        String playerColor = "BLACK";
         ChessBoard defaultBoard = new ChessBoard();
         defaultBoard.resetBoard();
         drawChessboard(printOut, playerColor, defaultBoard);
@@ -29,11 +31,17 @@ public class DrawBoard {
 
     }
 
-    private static void drawChessboard(PrintStream printOut, String playerColor, ChessBoard defaultBoard) {
+    private static void drawChessboard(PrintStream printOut, String playerColor, ChessBoard chessBoard) {
         drawHeader(printOut, playerColor);
-//        for (int i = 0; i < 8; i++) {
-//            drawRow(printOut, playerColor, defaultBoard);
-//        }
+        if (playerColor.equals("WHITE")) {
+            for (int i = 8; i > 0; i--) {
+                drawRow(printOut, playerColor, chessBoard, i);
+            }
+        } else {
+            for (int i = 1; i < 9; i++) {
+                drawRow(printOut, playerColor, chessBoard, i);
+            }
+        }
         drawHeader(printOut, playerColor);
     }
 
@@ -58,5 +66,36 @@ public class DrawBoard {
             printOut.print(EMPTY);
             printOut.println();
         }
+    }
+
+    private static void drawRow(PrintStream printOut, String playerColor, ChessBoard chessBoard, int row) {
+        drawRowNum(printOut, row);
+        boolean whiteTile;
+        if ((row % 2) == 0) {
+            whiteTile = false;
+        } else {
+            whiteTile = true;
+        }
+        for (int i = 0; i < 8; i++) {
+            if (whiteTile) {
+                printOut.print(SET_BG_COLOR_LIGHT_GREY);
+                whiteTile = false;
+            } else {
+                printOut.print(SET_BG_COLOR_DARK_GREY);
+                whiteTile = true;
+            }
+            ChessPiece piece = chessBoard.getPiece(new ChessPosition(row, i));
+            if (piece != null) {
+                printOut.print(EMPTY);
+            }
+        }
+        drawRowNum(printOut, row);
+        printOut.println();
+    }
+
+    private static void drawRowNum(PrintStream printOut, int row) {
+        printOut.print(SET_BG_COLOR_WHITE);
+        printOut.print(SET_TEXT_COLOR_BLACK);
+        printOut.print(" " + row + " ");
     }
 }
