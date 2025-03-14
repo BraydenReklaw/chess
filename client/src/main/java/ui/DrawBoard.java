8f58fd7d1babddba1eb7841ec5af5757fea16a55
BraydenW <braydenthewalker@gmail.com>
2025-03-14 12:39:48 -0600

    All Pieces print and align. Will need to remodel if unicode chess pieces want to be used

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -1,12 +1,12 @@
 package ui;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessPiece;
 import chess.ChessPosition;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 
 import static ui.EscapeSequences.*;
 
@@ -19,16 +19,10 @@ public class DrawBoard {
         defaultBoard.resetBoard();
         drawChessboard(printOut, playerColor, defaultBoard);
 
+        printOut.println();
+
         printOut.print(RESET_BG_COLOR);
         printOut.print(RESET_TEXT_COLOR);
-//        printOut.print(SET_BG_COLOR_RED);
-//        printOut.print(" Hello I am Baymax ");
-//        printOut.println();
-//        printOut.print(SET_TEXT_BOLD);
-//        printOut.print(" " + WHITE_KING + " ");
-//        printOut.print(" " + BLACK_KING + " ");
-
-
     }
 
     private static void drawChessboard(PrintStream printOut, String playerColor, ChessBoard chessBoard) {
@@ -50,20 +44,24 @@ public class DrawBoard {
         printOut.print(SET_BG_COLOR_WHITE);
         printOut.print(SET_TEXT_COLOR_BLACK);
         if (playerColor.equals("WHITE")) {
-            printOut.print(EMPTY);
+//            printOut.print(EMPTY);
+            printOut.print("   ");
             for(int i = 0; i < 8; i++) {
                 char c = header.charAt(i);
                 printOut.print(" " + c + " ");
             }
-            printOut.print(EMPTY);
+//            printOut.print(EMPTY);
+            printOut.print("   ");
             printOut.println();
         } else {
-            printOut.print(EMPTY);
+//            printOut.print(EMPTY);
+            printOut.print("   ");
             for(int i = 7; i >= 0; i--) {
                 char c = header.charAt(i);
                 printOut.print(" " + c + " ");
             }
-            printOut.print(EMPTY);
+//            printOut.print(EMPTY);
+            printOut.print("   ");
             printOut.println();
         }
     }
@@ -77,15 +75,31 @@ public class DrawBoard {
             whiteTile = true;
         }
         printOut.print(SET_TEXT_COLOR_WHITE);
-        for (int i = 1; i < 9; i++) {
-            if (whiteTile) {
-                printOut.print(SET_BG_COLOR_LIGHT_GREY);
-                whiteTile = false;
-            } else {
-                printOut.print(SET_BG_COLOR_DARK_GREY);
-                whiteTile = true;
+        if (playerColor.equals("WHITE")) {
+            whiteTile = !whiteTile;
+            for (int i = 1; i < 9; i++) {
+                if (whiteTile) {
+                    printOut.print(SET_BG_COLOR_LIGHT_GREY);
+                    whiteTile = false;
+                } else {
+                    printOut.print(SET_BG_COLOR_DARK_GREY);
+                    whiteTile = true;
+                }
+                drawTile(printOut, chessBoard, row, i);
+                printOut.print(RESET_TEXT_BOLD_FAINT);
             }
-            drawTile(printOut, chessBoard, row, i);
+        } else {
+            for (int i = 8; i > 0; i--) {
+                if (whiteTile) {
+                    printOut.print(SET_BG_COLOR_LIGHT_GREY);
+                    whiteTile = false;
+                } else {
+                    printOut.print(SET_BG_COLOR_DARK_GREY);
+                    whiteTile = true;
+                }
+                drawTile(printOut, chessBoard, row, i);
+                printOut.print(RESET_TEXT_BOLD_FAINT);
+            }
         }
         drawRowNum(printOut, row);
         printOut.println();
@@ -100,22 +114,41 @@ public class DrawBoard {
     private static void drawTile(PrintStream printOut, ChessBoard chessBoard, int row, int col) {
         ChessPiece piece = chessBoard.getPiece(new ChessPosition(row, col));
         if (piece == null) {
-            printOut.print(EMPTY);
+//            printOut.print(EMPTY);
+            printOut.print("   ");
         } else {
             String printArg = piece.getTeamColor() + "_" + piece.getPieceType();
+            if (piece.getTeamColor().equals(ChessGame.TeamColor.WHITE)) {
+                printOut.print(SET_TEXT_COLOR_WHITE);
+            } else {
+                printOut.print(SET_TEXT_COLOR_RED);
+                printOut.print(SET_TEXT_BOLD);
+            }
             switch (printArg) {
-                case "WHITE_KING" -> printOut.print(WHITE_KING);
-                case "WHITE_QUEEN" -> printOut.print(WHITE_QUEEN);
-                case "WHITE_ROOK" -> printOut.print(WHITE_ROOK);
-                case "WHITE_BISHOP" -> printOut.print(WHITE_BISHOP);
-                case "WHITE_KNIGHT" -> printOut.print(WHITE_KNIGHT);
-                case "WHITE_PAWN" -> printOut.print(WHITE_PAWN);
-                case "BLACK_KING" -> printOut.print(BLACK_KING);
-                case "BLACK_QUEEN" -> printOut.print(BLACK_QUEEN);
-                case "BLACK_ROOK" -> printOut.print(BLACK_ROOK);
-                case "BLACK_KNIGHT" -> printOut.print(BLACK_KNIGHT);
-                case "BLACK_BISHOP" -> printOut.print(BLACK_BISHOP);
-                case "BLACK_PAWN" -> printOut.print(BLACK_PAWN);
+//                case "WHITE_KING" -> printOut.print(WHITE_KING);
+//                case "WHITE_QUEEN" -> printOut.print(WHITE_QUEEN);
+//                case "WHITE_ROOK" -> printOut.print(WHITE_ROOK);
+//                case "WHITE_BISHOP" -> printOut.print(WHITE_BISHOP);
+//                case "WHITE_KNIGHT" -> printOut.print(WHITE_KNIGHT);
+//                case "WHITE_PAWN" -> printOut.print(WHITE_PAWN);
+//                case "BLACK_KING" -> printOut.print(BLACK_KING);
+//                case "BLACK_QUEEN" -> printOut.print(BLACK_QUEEN);
+//                case "BLACK_ROOK" -> printOut.print(BLACK_ROOK);
+//                case "BLACK_KNIGHT" -> printOut.print(BLACK_KNIGHT);
+//                case "BLACK_BISHOP" -> printOut.print(BLACK_BISHOP);
+//                case "BLACK_PAWN" -> printOut.print(BLACK_PAWN);
+                case "WHITE_KING" -> printOut.print(" K ");
+                case "WHITE_QUEEN" -> printOut.print(" Q ");
+                case "WHITE_ROOK" -> printOut.print(" R ");
+                case "WHITE_BISHOP" -> printOut.print(" B ");
+                case "WHITE_KNIGHT" -> printOut.print(" N ");
+                case "WHITE_PAWN" -> printOut.print(" P ");
+                case "BLACK_KING" -> printOut.print(" K ");
+                case "BLACK_QUEEN" -> printOut.print(" Q ");
+                case "BLACK_ROOK" -> printOut.print(" R ");
+                case "BLACK_KNIGHT" -> printOut.print(" N ");
+                case "BLACK_BISHOP" -> printOut.print(" B ");
+                case "BLACK_PAWN" -> printOut.print(" P ");
             }
         }
     }
