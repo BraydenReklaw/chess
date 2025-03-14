1aebc96d06066c395db5e18bd6c3984f40cb7582
BraydenW <braydenthewalker@gmail.com>
2025-03-14 10:49:34 -0600

    Top and Bottom headers print correctly

--- client/src/main/java/ui/DrawBoard.java
+++ client/src/main/java/ui/DrawBoard.java
@@ -31,9 +31,32 @@ public class DrawBoard {
 
     private static void drawChessboard(PrintStream printOut, String playerColor, ChessBoard defaultBoard) {
         drawHeader(printOut, playerColor);
-        for (int i = 0; i < 8; i++) {
-            drawRow(printOut, playerColor, defaultBoard);
-        }
+//        for (int i = 0; i < 8; i++) {
+//            drawRow(printOut, playerColor, defaultBoard);
+//        }
         drawHeader(printOut, playerColor);
+    }
+
+    private static void drawHeader(PrintStream printOut, String playerColor) {
+        String header = "abcdefgh";
+        printOut.print(SET_BG_COLOR_WHITE);
+        printOut.print(SET_TEXT_COLOR_BLACK);
+        if (playerColor.equals("WHITE")) {
+            printOut.print(EMPTY);
+            for(int i = 0; i < 8; i++) {
+                char c = header.charAt(i);
+                printOut.print(" " + c + " ");
+            }
+            printOut.print(EMPTY);
+            printOut.println();
+        } else {
+            printOut.print(EMPTY);
+            for(int i = 7; i >= 0; i--) {
+                char c = header.charAt(i);
+                printOut.print(" " + c + " ");
+            }
+            printOut.print(EMPTY);
+            printOut.println();
+        }
     }
 }
