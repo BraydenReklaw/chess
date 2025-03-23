2e0269166caeb61acd92e7a3bdfb11b3ca84bfde
BraydenW <braydenthewalker@gmail.com>
2025-03-23 15:50:18 -0600

    Trying to fix a compilation issue

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,12 +1,12 @@
 import chess.*;
 
-import server.Server;
-import ui.UI;
+//import server.Server;
+//import ui.UI;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
 
